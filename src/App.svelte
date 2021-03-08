fe0613291e5c71228f32d22e82fd908ba579c346
jasonlutterloh <22846656+jasonlutterloh@users.noreply.github.com>
2021-03-07 18:23:16 -0600

    Websocket functionality

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,29 @@
 	import Table from "./components/Table.svelte";
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
+import { onMount } from 'svelte';
 
 	// Get comma separated list of holdings
-	// const commaSeparatedHoldings = $wallet.map((holding)=> holding.id).toString();
+	const commaSeparatedHoldings = $wallet.map((holding)=> holding.id).toString();
 	// const holdingsArray = $wallet.map((holding)=> holding.symbol);
+
+	onMount(async () => {
+		const pricesWs = new WebSocket('wss://ws.coincap.io/prices?assets=' + commaSeparatedHoldings)
+
+    pricesWs.onmessage = function (msg) {
+    	let priceData = JSON.parse(msg.data);
+			for (const currencyId in priceData) {
+				const price = priceData[currencyId];
+				let modifiedWallet = $wallet;
+				modifiedWallet.forEach(walletHolding => {
+					if (walletHolding.id === currencyId){
+						walletHolding.priceUsd = price;
+					}
+				});
+				wallet.set(modifiedWallet);
+			}
+    }
+	});
 </script>
 
 <Header />
