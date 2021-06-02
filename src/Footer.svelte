0db7350b023dd691b584d5a6fa15fa120cc8a76b
jasonlutterloh <22846656+jasonlutterloh@users.noreply.github.com>
2021-06-02 07:05:49 -0500

    Updated footer

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,17 +1,18 @@
 <script>
-	import {slide} from "svelte/transition";
+		import {slide} from "svelte/transition";
 
-	let isExpanded = false;
+let isExpanded = false;
 </script>
 
 <footer>
 	<button on:click={()=> isExpanded = !isExpanded}>App Info</button>
 	{#if isExpanded}
 	<div transition:slide>
-		<p>HODL Monitor is an open source project. <a href="https://github.com/jasonlutterloh/hodltracker">GitHub</a></p>
-		<p>The app was built as a hobby and no support or warranty is provided. Accuracy is not guaranteed. <a target="_blank" href="https://coingecko.com/en/api">Powered by CoinGecko API</a>.</p>
-	</div>
-	{/if}
+	<p>HODL Monitor is an open source project. <a href="https://github.com/jasonlutterloh/hodltracker">GitHub</a></p>
+	<p>The app was built as a hobby and no support, liability, or warranty is provided. Accuracy is not guaranteed.</p>
+	<p>Privacy Promise: This app does not collect/store any data remotely, show ads, or use any analytics software. The app is <a target="_blank" href="https://coingecko.com/en/api">Powered by CoinGecko API</a> (to which it will pass a list of symbols) for pricing data and does rely on Google Fonts for, well, fonts.</p>
+</div>
+{/if}
 </footer>
 
 <style>
