f0e8cb47d5bc3810609bb87882e8f9490175bf9a
jasonlutterloh <22846656+jasonlutterloh@users.noreply.github.com>
2021-06-03 07:42:48 -0500

    Added privacy policy

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,38 +1,17 @@
 <script>
-		import {slide} from "svelte/transition";
 
-let isExpanded = false;
 </script>
 
 <footer>
-	<button on:click={()=> isExpanded = !isExpanded}>App Info</button>
-	{#if isExpanded}
-	<div transition:slide>
-	<p>HODL Monitor is an open source project. <a href="https://github.com/jasonlutterloh/hodltracker">GitHub</a></p>
+	<p>HODL Monitor is an open source project. <a href="https://github.com/jasonlutterloh/hodltracker" target="_blank">GitHub</a></p>
 	<p>The app was built as a hobby and no support, liability, or warranty is provided. Accuracy is not guaranteed.</p>
-	<p>Privacy Promise: This app does not collect/store any data remotely, show ads, or use any analytics software. The app is <a target="_blank" href="https://coingecko.com/en/api">Powered by CoinGecko API</a> (to which it will pass a list of symbols) for pricing data and uses Google Fonts for, well, fonts.</p>
-</div>
-{/if}
+	<p><a href="/privacypolicy.html" target="_blank">Privacy Policy and Terms of Use</a></p>
 </footer>
 
 <style>
 	footer{
 		font-size: .8em;
 		text-align: center;
-	}
-	button{
-		background: none;
-		border: 0;
-		cursor: pointer;
-		text-align: center;
-		padding: 1em;
-		text-transform: uppercase;
-		color: var(--text-color);
-		opacity: .6;
-	}
-	div{
-		margin: 1em;
-		flex: 1 1 100%;
 	}
 	a{
 		color: var(--text-color);
