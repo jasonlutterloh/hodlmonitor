d444a4fc9480ce0cc902edb5cbad910e98a0d45d
jasonlutterloh <22846656+jasonlutterloh@users.noreply.github.com>
2021-12-08 18:15:44 -0600

    feature: removed decimal rounding for dollar amounts less than 1

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,19 @@
+// TODO: Write some unit tests for this! (I know, I know... I'm being lazy.)
 export const getDollarDisplayValue = (text) => {
   if (text) {
-    text = text.toFixed(2);
-    text = text.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    let makePretty = false;
+    const decimalIndex = text.toString().indexOf(".");
+    if (decimalIndex >= 1) {
+      const numberBeforeDecimal = parseInt(text.toString().substring(0, decimalIndex));
+      makePretty = numberBeforeDecimal > 0 ? true : false;
+    } else {
+      makePretty = true;
+    }
+    
+    if (makePretty) {
+      text = text.toFixed(2);
+      text = text.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
   } else {
     text = 0;
   }
